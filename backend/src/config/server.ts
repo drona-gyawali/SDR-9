59fa09579c73ea505ace897bb6f640f495e9792d
Dorna Raj Gyawali <dronarajgyawali@gmail.com>
2026-03-16 11:18:34 +0545

    feat(api): added health endpoint

--- backend/src/config/server.ts
+++ backend/src/config/server.ts
@@ -3,6 +3,7 @@ import { conf, corsOption } from '../config/conf';
 import http from 'http';
 import socketManager from '../manager/socketManager';
 import emailRoutes from '../routes';
+import healthRoutes from "../routes"
 import cors from 'cors';
 
 const app: Express = express();
@@ -14,6 +15,7 @@ const server = http.createServer(app);
 socketManager.Init(server);
 
 app.use('/api/v1/', emailRoutes);
+app.use('api/v1/health', healthRoutes )
 
 server.listen(conf.port, '0.0.0.0', () => {
   console.log('Server is running on: ', conf.port);
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { SendLink } from '../controller/email';
-
+import { health } from '../controller/health';
 const router = Router();
 
 router.post('/send/email', SendLink);
+router.get('/health', health)
 
 export default router;
